bf24b64ac6ddf7e160ce7c11faa0d20f5bc8b81b
devcat37 <develop.fedorov@gmail.com>
2021-10-26 17:21:42 +0300

    добавил карты во вторую вкладку

---
+++ lib/internal/pages/planet_page/planet_page.dart
@@ -0,0 +1,14 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:treearth/presentation/pages/planet_page/planet_page_view.dart';
+
+class PlanetPage extends StatelessWidget {
+  const PlanetPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const PlanetPageView();
+  }
+}
